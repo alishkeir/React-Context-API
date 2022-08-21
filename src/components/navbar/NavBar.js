6a0b4f9e90b07f212e1e8513ac742cbbf41efb1a
alishkeir <alishkeir_@hotmail.com>
2022-08-21 21:34:08 +0300

    Changed NavBar to Hooks

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,69 +1,31 @@
+import { useContext } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import { AuthContext } from '../../contexts/AuthContext';
 
-import React, { Component } from 'react';
+const NavBar = () => {
+    const { isDarkTheme, darkTheme, lightTheme } = useContext(ThemeContext);
+    const { isLoggedIn, changeAuthStatus } = useContext(AuthContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
 
-class NavBar extends Component {
-    // how we set what context we need to use in a component
-    // static contextType = ThemeContext;
-
-    render() {
-        return (
-            <>
-                {/* another way to use context data using context consumer */}
-                <AuthContext.Consumer>
-                    {(authContext) => {
-                        return (
-                            <ThemeContext.Consumer>
-                                {(themeContext) => {
-                                    const {
-                                        isDarkTheme,
-                                        darkTheme,
-                                        lightTheme,
-                                    } = themeContext;
-                                    const theme = isDarkTheme
-                                        ? darkTheme
-                                        : lightTheme;
-
-                                    const { isLoggedIn, changeAuthStatus } =
-                                        authContext;
-                                    return (
-                                        <nav
-                                            style={{
-                                                background: theme.background,
-                                                color: theme.text,
-                                                height: '120px',
-                                            }}
-                                        >
-                                            <h2 style={{ textAlign: 'center' }}>
-                                                Ali Shkeir
-                                            </h2>
-                                            <p onClick={changeAuthStatus}>
-                                                {isLoggedIn
-                                                    ? 'Logged in'
-                                                    : 'Logged out'}
-                                            </p>
-                                            <div className='ui three buttons'>
-                                                <button className='ui button'>
-                                                    Overview
-                                                </button>
-                                                <button className='ui button'>
-                                                    Contact
-                                                </button>
-                                                <button className='ui button'>
-                                                    Support
-                                                </button>
-                                            </div>
-                                        </nav>
-                                    );
-                                }}
-                            </ThemeContext.Consumer>
-                        );
-                    }}
-                </AuthContext.Consumer>
-            </>
-        );
-    }
-}
+    return (
+        <nav
+            style={{
+                background: theme.background,
+                color: theme.text,
+                height: '120px',
+            }}
+        >
+            <h2 style={{ textAlign: 'center' }}>Ali Shkeir</h2>
+            <p onClick={changeAuthStatus}>
+                {isLoggedIn ? 'Logged in' : 'Logged out'}
+            </p>
+            <div className='ui three buttons'>
+                <button className='ui button'>Overview</button>
+                <button className='ui button'>Contact</button>
+                <button className='ui button'>Support</button>
+            </div>
+        </nav>
+    );
+};
 
 export default NavBar;
