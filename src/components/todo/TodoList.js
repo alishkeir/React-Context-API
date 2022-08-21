e6cd9edc848a435a882484cd7bedd786081bf244
alishkeir <alishkeir_@hotmail.com>
2022-08-21 21:21:46 +0300

    Changed TodoList to Hooks

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
-export class TodoList extends Component {
-    // how we set what context we need to use in a component
-    static contextType = ThemeContext;
+const TodoList = () => {
+    const { isDarkTheme, darkTheme, lightTheme, changeTheme } =
+        useContext(ThemeContext);
 
-    render() {
-        const { isDarkTheme, darkTheme, lightTheme, changeTheme } =
-            this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
+    const theme = isDarkTheme ? darkTheme : lightTheme;
 
-        return (
-            <div
-                style={{
-                    background: theme.background,
-                    color: theme.text,
-                    textAlign: 'center',
-                }}
-            >
-                <p className='item'>Plan the family trip</p>
-                <p className='item'>Go shopping</p>
-                <p className='item'>Go for a walk</p>
-                <button className='ui button primary' onClick={changeTheme}>
-                    Toggle Theme
-                </button>
-            </div>
-        );
-    }
-}
+    return (
+        <div
+            style={{
+                background: theme.background,
+                color: theme.text,
+                textAlign: 'center',
+            }}
+        >
+            <p className='item'>Plan the family trip</p>
+            <p className='item'>Go shopping</p>
+            <p className='item'>Go for a walk</p>
+            <button className='ui button primary' onClick={changeTheme}>
+                Toggle Theme
+            </button>
+        </div>
+    );
+};
 
 export default TodoList;
